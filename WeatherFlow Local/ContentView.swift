1b76c10c86a21bd9af59b26c16f880ba61df5248
hrbrmstr <bob@rud.is>
2022-01-29 08:01:04 -0500

    Initial Commit

---
+++ WeatherFlow Local/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherFlow Local
+//
+//  Created by boB Rudis on 1/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherFlow Local/WeatherFlow_LocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherFlow_LocalApp.swift
+//  WeatherFlow Local
+//
+//  Created by boB Rudis on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherFlow_LocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
